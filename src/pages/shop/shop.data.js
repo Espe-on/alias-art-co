3a026768c626bd0fc58fec9459443d8542dd0828
espe-on <contact@josephcrawley.dev>
2020-06-17 13:00:19 +0100

    added data for shop items and sections

---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,195 @@
+﻿const SHOP_DATA = [
+    {
+        id: 1,
+        title: "Artwork",
+        routeName: "artwork",
+        items: [
+            {
+                id: 1,
+                name: "Abstract Painting",
+                imageUrl: "https://i.ibb.co/Z8bCvcS/abstract.jpg",
+                price: 400
+            },
+            {
+                id: 2,
+                name: "Nature Painting",
+                imageUrl: "https://i.ibb.co/Fz0cG7W/nature.jpg",
+                price: 350
+            },
+            {
+                id: 3,
+                name: "Pre-Raphaelite Painting",
+                imageUrl: "https://i.ibb.co/TbVftcp/preraphaelite.jpg",
+                price: 1000
+            },
+            {
+                id: 4,
+                name: "Rococo Painting",
+                imageUrl: "https://i.ibb.co/XsPZSpS/rococo.jpg",
+                price: 2000
+            },
+            {
+                id: 5,
+                name: "Still Life Painting",
+                imageUrl: "https://i.ibb.co/fF4PJ7k/stilllife.jpg",
+                price: 200
+            },
+        ]
+    },
+    {
+        id: 2,
+        title: "Prints",
+        routeName: "prints",
+        items: [
+            {
+                id: 6,
+                name: "Abstract Poster",
+                imageUrl: "https://i.ibb.co/rxQSxwF/abstract-poster.jpg",
+                price: 40
+            },
+            {
+                id: 7,
+                name: "Nature Poster",
+                imageUrl: "https://i.ibb.co/VWKcLvD/nature-poster.jpg",
+                price: 40
+            },
+            {
+                id: 8,
+                name: "Pre-Raphaelite Poster",
+                imageUrl: "https://i.ibb.co/t8yRHR0/preraphaelite-poster.jpg",
+                price: 40
+            },
+            {
+                id: 9,
+                name: "Rococo Poster",
+                imageUrl: "https://i.ibb.co/7NyP4h4/rococo-poster.jpg",
+                price: 40
+            },
+            {
+                id: 10,
+                name: "Still Life Poster",
+                imageUrl: "https://i.ibb.co/C7yByf5/stilllife-poster.jpg",
+                price: 40
+            },
+        ]
+    },
+    {
+        id: 3,
+        title: "Cards",
+        routeName: "cards",
+        items: [
+            {
+                id: 11,
+                name: "Vintage Alaska Postcard",
+                imageUrl: "https://i.ibb.co/brHPvrF/alaska.jpg",
+                price: 2
+            },
+            {
+                id: 12,
+                name: "Vintage Hawaii Postcard",
+                imageUrl: "https://i.ibb.co/M96ZZYC/hawaii.jpg",
+                price: 2
+            },
+            {
+                id: 13,
+                name: "Vintage Key West Postcard",
+                imageUrl: "https://i.ibb.co/BGhLSQc/keywest.jpg",
+                price: 2
+            },
+            {
+                id: 14,
+                name: "Vintage Los Angeles Postcard",
+                imageUrl: "https://i.ibb.co/7Ys3fKm/losangeles.jpg",
+                price: 2
+            },
+            {
+                id: 15,
+                name: "Vintage New York Postcard",
+                imageUrl: "https://i.ibb.co/hZ3X9Tn/newyork.jpg",
+                price: 2
+            },
+            {
+                id: 16,
+                name: "Vintage Vermont Postcard",
+                imageUrl: "https://i.ibb.co/PsH2fRL/vermont.jpg",
+                price: 2
+            },
+        ]
+    },
+    {
+        id: 4,
+        title: "Pins",
+        routeName: "pins",
+        items: [
+            {
+                id: 17,
+                name: "Daria Pins",
+                imageUrl: "https://i.ibb.co/WBHW5S9/daria.jpg",
+                price: 5
+            },
+            {
+                id: 18,
+                name: "Hippie Pins",
+                imageUrl: "https://i.ibb.co/3RVcRWr/hippie.jpg",
+                price: 10
+            },
+            {
+                id: 19,
+                name: "Pride Pins",
+                imageUrl: "https://i.ibb.co/CmFFTF5/pride.jpg",
+                price: 10
+            },
+            {
+                id: 20,
+                name: "Mickey Mouse Pin",
+                imageUrl: "https://i.ibb.co/6ZVbkjQ/mickeymouse.jpg",
+                price: 3
+            },
+            {
+                id: 21,
+                name: "Simpsons Pins",
+                imageUrl: "https://i.ibb.co/xsqBq66/simpsons.jpg",
+                price: 10
+            },
+        ]
+    },
+    {
+        id: 5,
+        title: "T-Shirts",
+        routeName: "tshirts",
+        items: [
+            {
+                id: 22,
+                name: "Peace T-Shirt",
+                imageUrl: "https://i.ibb.co/HDzqh25/peace.jpg",
+                price: 30
+            },
+            {
+                id: 23,
+                name: "Dreamer T-Shirt",
+                imageUrl: "https://i.ibb.co/3W03bFK/dreamer.jpg",
+                price: 30
+            },
+            {
+                id: 24,
+                name: "Snake T-Shirt",
+                imageUrl: "https://i.ibb.co/dM56pLs/snake.jpg",
+                price: 30
+            },
+            {
+                id: 25,
+                name: "Sound Guy T-Shirt",
+                imageUrl: "https://i.ibb.co/3RJDKpv/soundguy.jpg",
+                price: 30
+            },
+            {
+                id: 26,
+                name: "Lighthouse T-Shirt",
+                imageUrl: "https://i.ibb.co/17tSkb8/lighthouse.jpg",
+                price: 30
+            },
+        ]
+    }
+];
+
+export default SHOP_DATA;
