d7340be0b99fda61b809546261f937d85133b03e
espe-on <contact@josephcrawley.dev>
2020-06-18 18:11:49 +0100

    implemented firebase utils, included ability to store new users in DB

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,16 +10,40 @@ const config = {
     storageBucket: "alias-art-co-db.appspot.com",
     messagingSenderId: "138228944882",
     appId: "1:138228944882:web:c6ec24025efe472b1ca56b",
-    measurementId: "G-YX5Z4HNDWV"
 };
 
 firebase.initializeApp(config);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+};
+
 export const auth = firebase.auth();
-export const firestore = firebase.firestore
+export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({prompt : "select_account" });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
