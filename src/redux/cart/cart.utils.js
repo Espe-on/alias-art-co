64f946c56746fe481e1e15ea5f40a9898b649aac
espe-on <contact@josephcrawley.dev>
2020-08-12 15:06:40 +0100

    added remove item functionality to cart reducer

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -3,13 +3,30 @@
         cartItem => cartItem.id === cartItemToAdd.id
     );
 
-    if(existingCartItems) {
+    if (existingCartItems) {
         return cartItems.map(cartItem =>
-        cartItem.id === cartItemToAdd.id
-        ? { ...cartItem, quantity: cartItem.quantity + 1}
-        : cartItem
+            cartItem.id === cartItemToAdd.id
+                ? {...cartItem, quantity: cartItem.quantity + 1}
+                : cartItem
         )
     }
 
-    return [...cartItems, {...cartItemToAdd, quantity: 1}]
+    return [...cartItems, {...cartItemToAdd, quantity: 1}];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItems = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    );
+    if (existingCartItems.quantity === 1) {
+        return cartItems.filter(cartItem.id !== cartItemToRemove.id)
+    }
+
+    return cartItems.map(
+        cartItem =>
+            cartItem.id === cartItemToRemove.id ?
+                {...cartItem, quantity: cartItem.quantity - 1}
+                :
+                cartItem
+    );
 }
