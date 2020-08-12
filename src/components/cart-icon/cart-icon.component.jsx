7c288302fede23ad904e9d51587dee2f88640a74
espe-on <contact@josephcrawley.dev>
2020-08-12 13:39:42 +0100

    cart-dropdown with selectors and App.js routing fix

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,18 +1,18 @@
-﻿import React from "react";
-import {connect} from "react-redux"
-import{createStructuredSelector} from "reselect";
+﻿import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import {toggleCartHidden} from "../../redux/cart/cart.actions";
-import {selectCartItemsCount} from "../../redux/cart/cart.selector";
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selector';
 
-import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg";
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
-import "./cart-icon.styles.scss";
+import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartHidden, itemCount}) => (
-    <div className="cart-icon" onClick={toggleCartHidden}>
-        <ShoppingIcon className="shopping-icon"/>
-        <span className="item-count">{itemCount}</span>
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
+    <div className='cart-icon' onClick={toggleCartHidden}>
+        <ShoppingIcon className='shopping-icon' />
+        <span className='item-count'>{itemCount}</span>
     </div>
 );
 
@@ -21,7 +21,7 @@ const mapDispatchToProps = dispatch => ({
 });
 
 const mapStateToProps = createStructuredSelector({
-    itemCount: selectCartItemsCount,
+    itemCount: selectCartItemsCount
 });
 
 export default connect(
