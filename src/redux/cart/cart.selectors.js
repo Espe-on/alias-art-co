14730c55d6dc729f48464897746abcfbcb199309
espe-on <contact@josephcrawley.dev>
2020-08-12 14:13:07 +0100

    implemented selectCarTotal selector

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -15,7 +15,17 @@ export const selectCartHidden = createSelector(
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems =>
-        cartItems.reduce((accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
+        cartItems.reduce((accumalatedQuantity, cartItem) =>
+            accumalatedQuantity + cartItem.quantity,
             0
         )
 );
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems =>
+        cartItems.reduce((accumalatedQuantity, cartItem) =>
+            accumalatedQuantity + cartItem.quantity * cartItem.price,
+            0
+        )
+)
